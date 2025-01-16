a70b16d9cb180cc0d0de2f076c462037e1bc0c2c
WeepingOphelia <workman.calvin@gmail.com>
2025-01-16 13:14:15 -0500

    player controls

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -7,12 +8,18 @@ def main():
   print(f"Screen height: {SCREEN_HEIGHT}")
   pygame.init
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    player.update(dt)
     screen.fill(000000)
+    player.draw(screen)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
